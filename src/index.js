c1ce4b67aa68465191a51d6b0846d84c477ae7a1
esdrassantos06 <esdrasirion1@gmail.com>
2025-03-07 01:57:47 +0000

    removed error e combined

--- src/index.js
+++ src/index.js
@@ -12,10 +12,6 @@ dotenv.config();
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
-    transports: [
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' })
-    ]
 });
 
 if (process.env.NODE_ENV !== 'production') {
