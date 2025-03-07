f8e40075cff2fa1a6ab51fb08eeef0cb21d3aeb3
esdrassantos06 <esdrasirion1@gmail.com>
2025-03-07 18:50:48 +0000

    FIXING AUTHENTICATE ON REGISTER

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,10 +2,13 @@ import { Router } from 'express';
 import { body } from 'express-validator';
 import authController from '../controllers/authController.js';
 import validate from '../middlewares/validator.js';
+import { authenticate, authorize } from '../middlewares/auth.js';
 
 const router = Router();
 
 router.post('/register',
+    authenticate,
+    authorize('admin'),
     body('username').isString().isLength({ min: 3, max: 30 }).withMessage('Username must be between 3 and 30 characters'),
     body('email').isEmail().withMessage('Valid email is required'),
     body('password').isString().isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
